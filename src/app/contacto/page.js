ec6549d45db0051fc8d4c61d73a8b79f317c1c2e
Stiph01 <71543061@continental.edu.pe>
2025-10-16 18:57:47 -0500

    Agregando estructura de proyecto

---
+++ src/app/contacto/page.js
@@ -0,0 +1,7 @@
+export default function Contacto(){
+    return(
+        <div>
+            <h1>Contactanos</h1>
+        </div>
+    )
+}
---
+++ src/app/servicios/page.js
@@ -0,0 +1,7 @@
+export default function Servicios (){
+    return (
+        <div>
+            <h1>Nuestros servicios</h1>
+        </div>
+    )
+}
